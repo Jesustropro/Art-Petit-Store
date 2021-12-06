feff49924b9d38ee081db87d2393b1817b0b4b80
ignacio2194 <82918800+ignacio2194@users.noreply.github.com>
2021-12-06 17:34:10 -0300

    logica del formulario terminada
    se aplico una expresion regular para validar el email y optimizacion del codigo

--- js/formulario.js
+++ js/formulario.js
@@ -5,50 +5,20 @@ const iconoForm = document.querySelector("#nombreUser");
 const iconoFormEmail = document.querySelector("#emailUser");
 const estadoFormulario = document.querySelector(".formularioEstado");
 const estadoFormularioEmail = document.querySelector(".formularioEstadoEmail");
+const exprReguEMail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
 
 
 const validarFormulario = (e) => {
   switch (e.target.name) {
     case "nombre":
       // en el caso de que el nombre tenga 3 digitos o menos pone la clase siguiente  y quita la clase datoos correctos
-      if (e.target.value.length < 3) {
-        e.target.classList.add("errorDatos");
-        e.target.classList.remove("datosCorrectos");
-        estadoFormulario.classList.add('formularioEstadoActivo')
-        iconoForm.classList.remove('formularioEstadoCorrecto')
-        iconoForm.classList.add('formularioEstadoIncorrecto')
-      document.querySelector('#iconoError ').classList.add('fa-times')
-      } else {
-        // caso contrario elimina la clase errorDatos y setea clase datosCorrectos
-        e.target.classList.remove("errorDatos");
-        e.target.classList.add("datosCorrectos");
-        estadoFormulario.classList.add('formularioEstadoActivo')
-        iconoForm.classList.remove('formularioEstadoIncorrecto')
-        iconoForm.classList.add('formularioEstadoCorreto')
-        document.querySelector('#iconoError').classList.remove('fa-times')
-        document.querySelector('#iconoError').classList.add('fa-check')
-      }
+      validarDatosNombre(e)
 
       break;
     case "email":
-      if (e.target.value.length < 3) {
-        e.target.classList.add("errorDatos");
-        e.target.classList.remove("datosCorrectos");
-        estadoFormularioEmail.classList.add('formularioEstadoActivo')
-        iconoFormEmail.classList.remove('formularioEstadoCorrecto')
-        iconoFormEmail.classList.add('formularioEstadoIncorrecto')
-        document.querySelector('#iconoErrorEmail ').classList.add('fa-times')
-        document.querySelector('#iconoErrorEmail ').classList.remove('fa-check')
-        } else {
-          // caso contrario elimina la clase errorDatos y setea clase datosCorrectos
-          e.target.classList.remove("errorDatos");
-          e.target.classList.add("datosCorrectos");
-          estadoFormularioEmail.classList.add('formularioEstadoActivo')
-          iconoFormEmail.classList.remove('formularioEstadoIncorrecto')
-          iconoFormEmail.classList.add('formularioEstadoCorreto')
-          document.querySelector('#iconoErrorEmail ').classList.remove('fa-times')
-          document.querySelector('#iconoErrorEmail ').classList.add('fa-check')
-        }
+      // si es un email no pasa el test entonces marcalo como no valido , caso contrario marcalo como valido 
+      validarDatosEmail(e)
       break;
   }
 };
@@ -58,5 +28,45 @@ inputsFormulario.forEach((input) => {
 });
 
 
+const validarDatosNombre = (e) => {
+  if (e.target.value.length < 3 || e.target.value.length > 25) {
+    e.target.classList.add("errorDatos");
+    e.target.classList.remove("datosCorrectos");
+    estadoFormulario.classList.add('formularioEstadoActivo')
+    iconoForm.classList.remove('formularioEstadoCorrecto')
+    iconoForm.classList.add('formularioEstadoIncorrecto')
+    document.querySelector('#iconoError ').classList.add('fa-times')
+  } else {
+    // caso contrario elimina la clase errorDatos y setea clase datosCorrectos
+    e.target.classList.remove("errorDatos");
+    e.target.classList.add("datosCorrectos");
+    estadoFormulario.classList.add('formularioEstadoActivo')
+    iconoForm.classList.remove('formularioEstadoIncorrecto')
+    iconoForm.classList.add('formularioEstadoCorreto')
+    document.querySelector('#iconoError').classList.remove('fa-times')
+    document.querySelector('#iconoError').classList.add('fa-check')
+  }
+}
+const validarDatosEmail = (e) => {
+  if (exprReguEMail.test(e.target.value) === false) {
+    e.target.classList.add("errorDatos");
+    e.target.classList.remove("datosCorrectos");
+    estadoFormularioEmail.classList.add('formularioEstadoActivo')
+    iconoFormEmail.classList.remove('formularioEstadoCorrecto')
+    iconoFormEmail.classList.add('formularioEstadoIncorrecto')
+    document.querySelector('#iconoErrorEmail').classList.add('fa-times')
+    document.querySelector('#iconoErrorEmail').classList.remove('fa-check')
+  } else {
+    // caso contrario elimina la clase errorDatos y setea clase datosCorrectos
+    e.target.classList.remove("errorDatos");
+    e.target.classList.add("datosCorrectos");
+    estadoFormularioEmail.classList.add('formularioEstadoActivo')
+    iconoFormEmail.classList.remove('formularioEstadoIncorrecto')
+    iconoFormEmail.classList.add('formularioEstadoCorreto')
+    document.querySelector('#iconoErrorEmail ').classList.remove('fa-times')
+    document.querySelector('#iconoErrorEmail ').classList.add('fa-check')
+  }
+
+}
 
 
